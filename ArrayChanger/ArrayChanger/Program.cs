589ce9f11a14ce44b342a8def05d02605b5a523d
olmytro <ironbro24@gmail.com>
2022-09-16 17:39:42 +0300

    first words
    first words

---
+++ ArrayChanger/ArrayChanger/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ArrayChanger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write rank");
+
+            Console.WriteLine("Write variable");
+            Console.WriteLine("Write ");
+        }
+    }
+}
